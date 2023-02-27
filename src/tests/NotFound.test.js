2cb49910cb2c6a2d80546a568d1600312f888e94
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-02-27 17:25:32 -0300

    Requisito 4: Testa o componente NotFound

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import { render, screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+
+describe('Testa o componente NotFound', () => {
+  test('Teste: O texto "Page request not found" é exibido em uma página que não existe', () => {
+    render(<NotFound />);
+
+    const title = screen.getByRole('heading', { level: 2, name: 'Page requested not found' });
+
+    expect(title).toBeInTheDocument();
+  });
+
+  test('Teste: Há uma tag img na página com o src desejado', () => {
+    render(<NotFound />);
+
+    const notFoundImg = screen.getByRole('img');
+
+    expect(notFoundImg).toBeInTheDocument();
+    expect(notFoundImg).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
