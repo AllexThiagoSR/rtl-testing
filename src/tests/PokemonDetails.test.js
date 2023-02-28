649783d05be3929bc0fd7101d77aa4fa5bb8719e
Allex Thiago <allexthiagogatinho502@gmail.com>
2023-02-28 15:17:53 -0300

    Requisito 7: Concluído

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,15 +1,18 @@
 import { screen } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 import renderWithRouter from '../renderWithRouter';
+import data from '../data';
+
+const path = '/pokemon/25';
 
 describe('Testa o componente PokemonDetails', () => {
   test('Teste: A página contém um elemento com o texto "Pikachu Details" ao entrar nos detalhos do pokémon Pikachu', () => {
-    const path = '/pokemon/25';
     const { history } = renderWithRouter(<App />);
     expect(history.location.pathname).toBe('/');
     const linkToPokemon = screen.getByRole('link', { name: 'More details' });
-    expect(linkToPokemon.href.includes('/pokemon/25')).toBe(true);
+    expect(linkToPokemon.href.includes(path)).toBe(true);
     act(() => {
       history.push(path);
     });
@@ -17,5 +20,31 @@ describe('Testa o componente PokemonDetails', () => {
     expect(screen.getByRole('heading', { name: 'Pikachu Details', level: 2 })).toBeInTheDocument();
     expect(linkToPokemon).not.toBeInTheDocument();
     expect(screen.getByRole('heading', { level: 2, name: 'Summary' }));
+    expect(screen.getByText(/electricity/i)).toBeInTheDocument();
+  });
+
+  test('Teste: Renderiza as informações dos mapas corretamente', () => {
+    const { foundAt } = data[0];
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push(path);
+    });
+    expect(screen.getByRole('heading', { level: 2, name: 'Game Locations of Pikachu' })).toBeInTheDocument();
+    foundAt.forEach(({ location, map }, index) => {
+      expect(screen.getByText(location)).toBeInTheDocument();
+      const mapsImages = screen.getAllByRole('img', { name: 'Pikachu location' });
+      expect(mapsImages[index]).toBeInTheDocument();
+      expect(mapsImages[index]).toHaveProperty('src', map);
+    });
+  });
+
+  test('Teste: É possível favoritar o pokemon pela página de detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+    act(() => {
+      history.push(path);
+    });
+    const favorite = screen.getByLabelText('Pokémon favoritado?');
+    userEvent.click(favorite);
+    expect(screen.getByRole('img', { name: 'Pikachu is marked as favorite' })).toBeInTheDocument();
   });
 });
